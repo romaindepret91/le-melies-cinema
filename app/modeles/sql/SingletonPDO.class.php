74fa11e68cefbdd579f69518a6c8847dfbd8ee02
romaindepret91 <romaindepret91@gmail.com>
2023-01-03 17:21:21 -0500

    Database variables configuration

--- app/modeles/sql/SingletonPDO.class.php
+++ app/modeles/sql/SingletonPDO.class.php
@@ -4,11 +4,11 @@ class SingletonPDO extends PDO
 {
     private static $instance = null;
 
-    const DB_SERVEUR  = 'localhost';
-    const DB_NOM      = 'cinema';
+    const DB_SERVEUR  = "us-cdbr-east-06.cleardb.net" ; //'localhost';
+    const DB_NOM      = "heroku_7c851397e4d5f5c"; //'cinema';
     const DB_DSN      = 'mysql:host='. self::DB_SERVEUR .';dbname='. self::DB_NOM.';charset=utf8'; 
-    const DB_LOGIN    = 'root';
-    const DB_PASSWORD = 'root';
+    const DB_LOGIN    = "bdc75edba29ccd"; //'root';
+    const DB_PASSWORD = "0c7e5cff"; //'root';
 
     private function __construct() {
         
